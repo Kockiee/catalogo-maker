2c39687e2447c3baac180c646c91f365dfc4c6d9
Miguel Eduardo <mgueli1208@gmail.com>
2024-04-26 16:34:30 -0300

    Update ScrollDownButton.js

--- src/app/components/ScrollDownButton.js
+++ src/app/components/ScrollDownButton.js
@@ -1,20 +1,23 @@
 'use client'
-
 import { HiArrowDown } from "react-icons/hi";
 
-export default function ScrollDownButton(destinyId) {
-    return (
-        <button
-        className="bg-cornflowerblue hover:opacity-70 rounded-full animate-bounce p-3"
-        onClick={() => {
-          const destiny = document.querySelector(destinyId);
-          window.scrollTo({
-            top: destiny.offsetTop,
-            behavior: 'smooth'
-          });
-        }}
-        >
-          <HiArrowDown className="text-lightcyan w-8 h-8 "/>
-        </button>
-    )
+export default function ScrollDownButton({ destinyId }) {
+  const handleClick = () => {
+    const destiny = document.querySelector(destinyId);
+    if (destiny) {
+      window.scrollTo({
+        top: destiny.offsetTop,
+        behavior: "smooth"
+      });
+    }
+  };
+
+  return (
+    <button
+      className="bg-cornflowerblue hover:opacity-70 rounded-full animate-bounce p-3"
+      onClick={handleClick}
+    >
+      <HiArrowDown className="text-lightcyan w-8 h-8" />
+    </button>
+  );
 }
